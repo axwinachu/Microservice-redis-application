37d930f7871416ef8845e514ffbc28c47ad3fb74
axwinachu <ceaswin146@gmail.com>
2026-02-14 11:48:19 +0530

    basic setups

---
+++ src/main/java/com/example/microservice_redis_application/facade/userFacade.java
@@ -0,0 +1,9 @@
+package com.example.microservice_redis_application.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class userFacade {
+}
---
+++ src/main/java/com/example/microservice_redis_application/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.microservice_redis_application.repository;
+
+import com.example.microservice_redis_application.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {}
