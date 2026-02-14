caab959652974217ce682fa0a33a3cb8f64d4d14
axwinachu <ceaswin146@gmail.com>
2026-02-14 11:35:54 +0530

    project base

---
+++ src/test/java/com/example/microservice_redis_application/MicroserviceRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice_redis_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
